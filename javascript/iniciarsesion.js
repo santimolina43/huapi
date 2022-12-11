195883165af205c82f17329f5d680ac45e5dcd5b
santimolina <molan_24@hotmail.com>
2022-12-11 01:11:07 -0300

    se mejora el inicio de sesion y se agrega la pagina perfil.html

--- javascript/iniciarsesion.js
+++ javascript/iniciarsesion.js
@@ -1,9 +1,15 @@
+const iniciarSesionPerfil = document.getElementById('iniciarSesionPerfil');
+const iniciarSesionPerfil2 = document.getElementById('iniciarSesionPerfil2');
+
+checkSession();
+
 const ingresar = document.getElementById("ingresar");
 const arrayClaves = [];
 const iniciarSesionInput = document.getElementById("iniciarSesionInput");
+let sessionBoo = sessionStorage.getItem('sessionIniciada');
 
-
-ingresar.addEventListener("click", (e) => {
+if (sessionBoo != 'true') {
+  ingresar.addEventListener("click", (e) => {
     e.preventDefault();
     const emailIngresado = document.getElementById("floatingInput").value; 
     const contraseñaIngresada = document.getElementById("floatingPassword").value;
@@ -16,6 +22,12 @@ ingresar.addEventListener("click", (e) => {
             let userIngresado = localStorage.getItem(clave);
             let usuarioObj = JSON.parse(userIngresado);
             if (usuarioObj.contraseña == contraseñaIngresada) {
+                setTimeout(() => {
+                  sessionStorage.setItem("sessionIniciada",true);
+                  checkSession();
+                  const modalbackdrop = document.getElementsByClassName('modal-backdrop');
+                  modalbackdrop[0].style.display = 'none';
+                }, 1500);
                 contraseñaDiv.className = "form-control";
                 iniciarSesionInput.innerHTML = `
                                 <div class="form-floating mb-3">
@@ -51,7 +63,6 @@ ingresar.addEventListener("click", (e) => {
                                                 `
                 contraseñaDiv.className = "form-control error-form";
             }
-            console.log(usuarioObj);
         } else {
             iniciarSesionInput.innerHTML = `
                                 <div class="form-floating mb-3">
@@ -73,3 +84,115 @@ ingresar.addEventListener("click", (e) => {
         }
     }
 })
+}
+
+/* Cerrar Sesion */
+const cerrarSesion = document.getElementById('cerrarSesion');
+
+if (sessionBoo == 'true') {
+  if (cerrarSesion !== null && cerrarSesion !== undefined) {
+    cerrarSesion.addEventListener('click', () => {
+        sessionStorage.setItem("sessionIniciada",false);
+        checkSession();
+        window.location = '../index.html';
+    })
+  }
+}
+
+
+/********* FUNCIONES ********/
+function checkSession() {
+  let sessionBoo = sessionStorage.getItem('sessionIniciada');
+
+  if (sessionBoo === 'true') {
+    if (iniciarSesionPerfil !== null && iniciarSesionPerfil !== undefined) {
+      iniciarSesionPerfil.innerHTML = `
+        <li class="menu__item">
+          <a class="nav-link active" aria-current="page"href="pantallas/perfil.html">Mi Perfil</a>
+        </li>
+                                        `
+    }
+    if (iniciarSesionPerfil2 !== null && iniciarSesionPerfil2 !== undefined) {
+      iniciarSesionPerfil2.innerHTML = `
+        <li class="menu__item">
+          <a class="nav-link active" aria-current="page"href="perfil.html">Mi Perfil</a>
+        </li>
+                                        `
+    }
+    return true;
+  } else {
+    if (iniciarSesionPerfil !== null && iniciarSesionPerfil !== undefined) {
+      iniciarSesionPerfil.innerHTML = `
+        <!-- Button trigger modal -->
+        <button type="button" class="menu__item sesion__button" data-bs-toggle="modal" data-bs-target="#exampleModal">
+            Iniciar sesión
+        </button>
+        <!-- Modal -->
+        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Ingresa tus datos</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body" id="iniciarSesionInput">
+                    <div class="form-floating mb-3">
+                        <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+                        <label for="floatingInput">Email</label>
+                      </div>
+                      <div class="form-floating">
+                        <input type="password" class="form-control" id="floatingPassword" placeholder="Contraseña">
+                        <label for="floatingPassword">Contraseña</label>
+                      </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-primary" id="ingresar">Iniciar sesión</button>
+                    <div class="modal-footer-registrate">
+                        <h6>¿No tienes cuenta aun? </h6>
+                        <a href="pantallas/registrarme.html"> Registrate</a>
+                    </div>
+                </div>
+            </div>
+            </div>
+        </div>
+                                        `
+    }
+    if (iniciarSesionPerfil2 !== null && iniciarSesionPerfil2 !== undefined) {
+      iniciarSesionPerfil2.innerHTML = `
+        <!-- Button trigger modal -->
+        <button type="button" class="menu__item sesion__button" data-bs-toggle="modal" data-bs-target="#exampleModal">
+            Iniciar sesión
+        </button>
+        <!-- Modal -->
+        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Ingresa tus datos</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body" id="iniciarSesionInput">
+                    <div class="form-floating mb-3">
+                        <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+                        <label for="floatingInput">Email</label>
+                      </div>
+                      <div class="form-floating">
+                        <input type="password" class="form-control" id="floatingPassword" placeholder="Contraseña">
+                        <label for="floatingPassword">Contraseña</label>
+                      </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-primary" id="ingresar">Iniciar sesión</button>
+                    <div class="modal-footer-registrate">
+                        <h6>¿No tienes cuenta aun? </h6>
+                        <a href="registrarme.html"> Registrate</a>
+                    </div>
+                </div>
+            </div>
+            </div>
+        </div>
+                                        `
+    }
+    return false;
+  }
+}
